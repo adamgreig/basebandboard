aed23bb1698a80349e4239d8ad89d943730aea61
Adam Greig <adam@adamgreig.com>
2017-11-13 03:40:18 +0000

    add some example matrices and packing script

---
+++ software/rnghunt/util/pack.py
@@ -0,0 +1,21 @@
+import sys
+import numpy as np
+
+lines = []
+
+for line in open(sys.argv[1]):
+    lines.append(line.strip())
+
+n = len(lines[0])
+print("Detected n={}".format(n))
+a = np.empty((n, n), dtype=np.uint8)
+for rowidx in range(n):
+    a[rowidx] = [int(x) for x in lines[rowidx]]
+print("Loaded matrix")
+print("Row weights:", a.sum(axis=1))
+print("Col weights:", a.sum(axis=0))
+
+packed = [np.where(a[i])[0].tolist() for i in range(n)]
+
+print("Packed:")
+print(packed)
