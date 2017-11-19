7e4c41e413c4d7c26d9d9e2a597ee8a0f06ac0b5
Adam Greig <adam@adamgreig.com>
2017-11-19 05:14:14 +0000

    add pll to clock dac up to 200MS/s

--- gateware/bbb/platform.py
+++ gateware/bbb/platform.py
@@ -2,8 +2,48 @@
 Platform class for the prototype ADC/DAC and other hardware.
 """
 
+from migen import Signal, Module, Instance
 from migen.build.generic_platform import Pins, IOStandard, Subsignal
 from migen.build.platforms import de0nano
+
+
+class PLL(Module):
+    """Helper for instantiating onboard PLLs"""
+    def __init__(self, period_in, name, divide_by, multiply_by):
+        self.clk_in = Signal()
+        self.clk_out = Signal()
+        self.specials += Instance("ALTPLL",
+                                  p_bandwidth_type="AUTO",
+                                  p_clk0_divide_by=divide_by,
+                                  p_clk0_duty_cycle=50,
+                                  p_clk0_multiply_by=multiply_by,
+                                  p_clk0_phase_shift="0",
+                                  p_compensate_clock="CLK0",
+                                  p_inclk0_input_frequency=int(period_in*1000),
+                                  p_intended_device_family="Cyclone IV E",
+                                  p_lpm_hint="CBX_MODULE_PREFIX={}_pll"
+                                             .format(name),
+                                  p_lpm_type="altpll",
+                                  p_operation_mode="NORMAL",
+                                  i_inclk=self.clk_in,
+                                  o_clk=self.clk_out,
+                                  i_areset=0,
+                                  i_clkena=0x3f,
+                                  i_clkswitch=0,
+                                  i_configupdate=0,
+                                  i_extclkena=0xf,
+                                  i_fbin=1,
+                                  i_pfdena=1,
+                                  i_phasecounterselect=0xf,
+                                  i_phasestep=1,
+                                  i_phaseupdown=1,
+                                  i_pllena=1,
+                                  i_scanaclr=0,
+                                  i_scanclk=0,
+                                  i_scanclkena=1,
+                                  i_scandata=0,
+                                  i_scanread=0,
+                                  i_scanwrite=0)
 
 
 _dac = [
--- gateware/top.py
+++ gateware/top.py
@@ -1,26 +1,29 @@
 from migen import Module, Signal, ClockDomain
 
-from bbb.platform import BBBPlatform
+from bbb.platform import BBBPlatform, PLL
 from bbb.tx import TX
 
 
 class Top(Module):
     def __init__(self, platform):
-        # Set up clocking. We'll just run at 50MHz to start with.
+        # Set up clocking.
         self.clk50 = platform.request("clk50")
+        self.sys_pll = PLL(20, "sys", 1, 4)
+        self.submodules += self.sys_pll
         self.clock_domains.sys = ClockDomain("sys")
-        self.comb += self.sys.clk.eq(self.clk50)
+        self.comb += self.sys_pll.clk_in.eq(self.clk50)
+        self.comb += self.sys.clk.eq(self.sys_pll.clk_out)
 
         # Set up the DAC and ADC peripherals
         self.dac = plat.request("dac")
-        self.comb += self.dac.clk.eq(~self.sys.clk)
+        self.comb += self.dac.clk.eq(self.sys.clk)
 
         # Create a transmitter.
         self.prbs_k = 9
         self.bit_en = Signal()
         self.shape_sel = Signal(5, reset=0)
         self.noise_en = Signal()
-        self.noise_var = Signal((4, True), reset=1)
+        self.noise_var = Signal((4, True), reset=10)
         self.tx = TX(self.prbs_k, self.bit_en, self.shape_sel,
                      self.noise_en, self.noise_var)
         self.submodules += self.tx
@@ -33,12 +36,18 @@ class Top(Module):
             self.noise_en.eq(sw[1]),
             self.shape_sel[0].eq(sw[2]),
             self.shape_sel[1].eq(sw[3]),
-            self.dac.data.eq(self.tx.x),
+            self.dac.data.eq(self.tx.x << 2),
         ]
         self.comb += [
             leds[0].eq(self.bit_en),
             leds[1].eq(self.noise_en),
+            leds[2].eq(self.shape_sel[0]),
+            leds[3].eq(self.shape_sel[1]),
         ]
+
+        # Output the bit clock somewhere too
+        gpio1 = plat.request("gpio_1")
+        self.comb += gpio1[0].eq(self.tx.shaper.prbsclk.clk)
 
 
 if __name__ == "__main__":
