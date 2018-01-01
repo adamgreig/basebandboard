f18ad91b6460a47ed267aa4da32ebf7dfa590a11
Adam Greig <adam@adamgreig.com>
2018-01-01 05:34:43 +0000

    wip lcd

--- gateware/bbb/rgb_lcd.py
+++ gateware/bbb/rgb_lcd.py
@@ -4,43 +4,145 @@
 Copyright 2017 Adam Greig
 """
 
-from migen import Module, Signal, FSM, If, NextState, NextValue, ClockDomain
+from migen import Module, Signal, Cat, If, ClockDomain, Memory
+from .axi3 import AXI3ToBRAM, BRAMToAXI3
 
 
 class RGBLCD(Module):
-    def __init__(self, lcd):
-        """
-        `lcd` has `data`, `pclk`, `hsync`, `vsync`, `de` members.
-        """
+    """
+    RGB LCD controller.
+    Reads a frame buffer over AXI3 starting from the given address and
+    outputs it to the LCD.
 
+    Currently hardcoded for 272x480 24-bit RGB LCDs.
+    """
+    def __init__(self, lcd, axi3_port, start_addr):
+        """
+        `lcd`: has `data`, `pclk`, `hsync`, `vsync`, `de` members.
+        `axi3_port`: an AXI3ReadPort connected to memory etc. will be mastered
+                     to command reads of screen data.
+        `start_addr`: the initial address of the frame buffer (for axi3)
+        """
         # Make up a pclk divider
         divider = Signal(4)
         self.sync += divider.eq(divider + 1)
         self.clock_domains.pclk = ClockDomain("pclk")
         self.comb += self.pclk.clk.eq(divider[3])
-        self.comb += lcd.pclk.eq(self.pclk.clk)
+        self.comb += lcd.pclk.eq(~self.pclk.clk)
 
+        # Store current col (pclk) and row (hsync) counts
         pcount = Signal(10)
         hcount = Signal(10)
+
+        # Make a counter to track the address of the current line,
+        # offset for the vertical back porch and compensate for latency.
+        axiaddr = Signal(start_addr.nbits)
+        self.comb += axiaddr.eq(start_addr + (4*480)*(hcount - 8))
+
+        # Make a BRAM to store one line worth
+        bram = Memory(32, 512)
+        bram_rp = bram.get_port()
+        bram_wp = bram.get_port(write_capable=True)
+        self.specials += [bram, bram_rp, bram_wp]
+
+        # Set up an AXI3 master to read data into the BRAM when triggered
+        # by the HSYNC signal
+        axitrig = Signal()
+        self.comb += axitrig.eq((lcd.hsync == 0)
+                                & (hcount > 7) & (hcount < 281))
+        axi_to_bram = AXI3ToBRAM(axi3_port, bram_wp, axitrig, axiaddr,
+                                 484, axi3_burst_length=4)
+        self.submodules += axi_to_bram
+
+        # Count up pclks and hsyncs, resetting at the end of each period
         self.sync.pclk += (
             If(
-                pcount == 525,
+                pcount == 524,
                 pcount.eq(0),
                 If(
-                    hcount == 288,
+                    hcount == 287,
                     hcount.eq(0))
                 .Else(hcount.eq(hcount + 1)))
             .Else(pcount.eq(pcount + 1))
         )
 
+        # Output hsyncs at the start of each line
         self.sync.pclk += If(pcount == 0,
-                             lcd.hsync.eq(1)).Else(lcd.hsync.eq(0))
-        self.sync.pclk += If((pcount > 40) & (pcount < 520) &
-                             (hcount > 8) & (hcount < 280),
+                             lcd.hsync.eq(0)).Else(lcd.hsync.eq(1))
+        # Output DE during the active area of each line
+        self.sync.pclk += If((pcount > 39) & (pcount < 521) &
+                             (hcount > 7) & (hcount < 281),
                              lcd.de.eq(1)).Else(lcd.de.eq(0))
+        # Output vsyncs at the start of each frame
         self.sync.pclk += If(hcount == 0,
-                             lcd.vsync.eq(1)).Else(lcd.vsync.eq(0))
+                             lcd.vsync.eq(0)).Else(lcd.vsync.eq(1))
 
-        self.comb += lcd.data[0:8].eq(pcount[1:9] - 20)
-        self.comb += lcd.data[8:16].eq(hcount[1:9] - 4)
-        self.comb += lcd.data[16:24].eq(0b00000000)
+        # Pixel data is the output of the BRAM at the current pixel count,
+        # adjusted to compensate for the back porch and memory read latency
+        self.comb += bram_rp.adr.eq(pcount - 41)
+        self.comb += lcd.data.eq(bram_rp.dat_r[0:24])
+
+
+class LCDPatternGenerator(Module):
+    def __init__(self, axi3_port, start_addr):
+        """
+        `axi3_port`: an AXI3WritePort connected to memory etc. will be mastered
+                     to command writes of test pattern data.
+        `start_addr`: the initial address of the frame buffer.
+        """
+        # Maintain row and column counters
+        row = Signal(9, reset=136)
+        col = Signal(9)
+        run = Signal()
+        self.sync += If(
+            col == 510,
+            col.eq(0),
+            If(
+                row == 272,
+                row.eq(0),
+            ).Else(
+                row.eq(row + 1)
+            )
+        ).Else(
+            col.eq(col + run)
+        )
+
+        # Make a BRAM to store one line worth.
+        # We give it a little slack at the end to simplify timing.
+        red = 0b00000000111111110000000000000000
+        grn = 0b11111111000000000000000000000000
+        blu = 0b00000000000000000000000011111111
+        wte = 0b11111111111111110000000011111111
+        initial = [wte]*2 + [0, 0, 0, grn, 0, 0, 0]*68 + [blu]*2
+        bram = Memory(32, 512, initial)
+        bram_rp = bram.get_port()
+        bram_wp = bram.get_port(write_capable=True)
+        self.specials += [bram, bram_rp, bram_wp]
+        self.comb += bram_wp.we.eq(0)
+
+        # Make a counter to track the address of the current line,
+        # offset for the vertical back porch and compensate for latency.
+        axiaddr = Signal(start_addr.nbits)
+        self.comb += axiaddr.eq(start_addr + (4*480)*row)
+
+        # Set up an AXI3 master to write data into the BRAM
+        bram_to_axi = BRAMToAXI3(axi3_port, bram_rp, col == 480, axiaddr,
+                                 484, axi3_burst_length=4)
+        self.comb += run.eq(bram_to_axi.ready)
+        self.submodules += bram_to_axi
+
+        # Generate test pattern
+        red = Signal(8)
+        grn = Signal(8)
+        blu = Signal(8)
+        #self.comb += bram_wp.dat_w[0:24].eq(Cat(red, grn, blu))
+        #self.comb += bram_wp.adr.eq(col - 8)
+        #self.comb += bram_wp.we.eq(1)
+
+        #self.comb += red.eq(((col & 0b1111)) << 7)
+        #self.comb += grn.eq(((col & 0b1111)) << 7)
+        #self.comb += blu.eq(((col & 0b1111)) << 7)
+
+        self.comb += red.eq(0b11111111)
+        self.comb += grn.eq(0b11111111)
+        self.comb += blu.eq(0b00000000)
--- gateware/lcd_demo.py
+++ gateware/lcd_demo.py
@@ -1,26 +1,55 @@
 from migen import Module, Signal, ClockDomain
 from bbb.platform import BBBPlatform, PLL
-from bbb.rgb_lcd import RGBLCD
+from bbb.rgb_lcd import RGBLCD, LCDPatternGenerator
+from bbb.sdram import SDRAM
+from bbb.axi3 import AXI3ReadPort, AXI3WritePort
 
 
 class Top(Module):
     def __init__(self, platform):
+        # Set up clocks.
+        # PLL the 50MHz to 100MHz, and PLL a -3ns phase shift for the SDRAM.
         self.clk50 = platform.request("clk50")
         sys_pll = PLL(20, "sys", 1, 2)
         self.submodules += sys_pll
         self.clock_domains.sys = ClockDomain("sys")
         self.comb += sys_pll.clk_in.eq(self.clk50)
         self.comb += self.sys.clk.eq(sys_pll.clk_out)
+        sdram_pll = PLL(20, "sdram", 1, 2, "ZERO_DELAY_BUFFER", -3000)
+        self.submodules += sdram_pll
+        self.clock_domains.sys_ps = ClockDomain("sys_ps")
+        self.comb += sdram_pll.clk_in.eq(self.clk50)
+        self.comb += self.sys_ps.clk.eq(sdram_pll.clk_out)
+        self.comb += platform.request("sdram_clock").eq(self.sys_ps.clk)
 
+        # SDRAM controller
+        timings = {
+            "powerup": 150*200,
+            "t_cac": 2,
+            "t_rcd": 3,
+            "t_rc": 10,
+            "t_ras": 7,
+            "t_rp": 3,
+            "t_mrd": 3,
+            "t_ref": 750,
+        }
+        axirp = AXI3ReadPort(1, 24, 32)
+        axiwp = AXI3WritePort(1, 24, 32)
+        sdram = plat.request("sdram")
+        sdramctl = SDRAM(axirp, axiwp, sdram, timings)
+        self.submodules += sdramctl
+        self.specials += sdramctl.dqt.get_tristate(sdram.dq)
+
+        # LCD controller
         display = platform.request("lcd")
         backlight = platform.request("lcd_backlight")
+        self.comb += backlight.eq(1)
+        self.comb += display.disp.eq(1)
+        startaddr = Signal(24)
+        self.submodules.lcd = RGBLCD(display, axirp, startaddr)
 
-        key0 = platform.request("key")
-        key1 = platform.request("key")
-        self.comb += backlight.eq(key0)
-        self.comb += display.disp.eq(key1)
-
-        self.submodules.lcd = RGBLCD(display)
+        # LCD test pattern generator
+        self.submodules.patgen = LCDPatternGenerator(axiwp, startaddr)
 
 
 if __name__ == "__main__":
